4aa31cbc7bf5296fdddddc1a98eacb599717b098
Seppo <seppo0010@gmail.com>
2010-09-12 12:10:39 -0300

    Photo uploads

--- sample2.py
+++ sample2.py
@@ -6,25 +6,98 @@ import stat
 import time
 import httplib
 import json
+import os
+import sys
+import ConfigParser
+import tempfile
+import urllib2
+from multipart import Multipart
 
 fuse.fuse_python_api = (0, 2)
 
 class MyFS(fuse.Fuse):
 	albums = {};
-	access_token = '<fill me>';
+	access_token = None;
+	tempfiles = {};
 
 	def __init__(self, *args, **kw):
 		fuse.Fuse.__init__(self, *args, **kw)
+		home = os.getenv('HOME');
+		if home == None:
+			sys.exit('No access token set');
+
+		try:
+			config = ConfigParser.ConfigParser()
+			config.read([home + '/.fbfuserc'])
+			self.access_token = config.get('facebook', 'access_token');
+			if self.access_token == None:
+				sys.exit('No access token set');
+		except ConfigParser.Error:
+			sys.exit('No access token set');
 
 	def getattr(self, path):
 		st = fuse.Stat()
-		st.st_mode = stat.S_IFDIR | 0755
-		st.st_nlink = 2
+		if path == '/' or path == '/photos' or (path.startswith('/photos') and path.count('/') == 2):
+			st.st_mode = stat.S_IFDIR | 0755
+			st.st_nlink = 2
+		else:
+			st.st_mode = stat.S_IFREG | 0755
+			st.st_nlink = 1
 		st.st_atime = int(time.time())
 		st.st_mtime = st.st_atime
 		st.st_ctime = st.st_atime
 
 		return st
+
+	def mknod(self, path, mode, dev):
+		return -EINVAL
+
+	def open(self, path, flags):
+		return 0
+
+	def release(self, path, flags):
+		tmp = self.tempfiles[path];
+		tmp.seek(0, os.SEEK_SET);
+		file = tmp.read();
+		m = Multipart()
+		m.field('access_token',self.access_token)
+		m.file('source','image',file,{'Content-Type':'image/jpeg'})
+		ct,body = m.get()
+
+		request = urllib2.Request('https://graph.facebook.com/' + self.albums[path[8:path.find('/', 8)]]['id'] + '/photos',body,{'Content-Type':ct});
+		reply = urllib2.urlopen(request)
+		tempfile = tmp.name;
+		tmp.close();
+		del self.tempfiles[path]
+		os.unlink = tempfile;
+
+	def write(self, path, buf, offset):
+		tmp = None;
+		if self.tempfiles.has_key(path):
+			tmp = self.tempfiles[path];
+		else:
+			self.tempfiles[path] = tmp = tempfile.NamedTemporaryFile();
+		tmp.seek(offset)
+		tmp.write(buf)
+		return len(buf);
+
+	def truncate(self, path, size):
+		return 0
+
+	def utime(self, path, times):
+		return 0
+
+	def mkdir(self, path, mode):
+		return 0
+
+	def rmdir(self, path):
+		return 0
+
+	def rename(self, pathfrom, pathto):
+		return 0
+
+	def fsync(self, path, isfsyncfile):
+		return 0
 
 	def readdir(self, path, offset):
 		for e in '.', '..':
